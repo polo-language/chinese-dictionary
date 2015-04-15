a14ff0565e24d345f0d602a211d3a0eec6a36404
Angelo Polo <polo.language@gmail.com>
2015-04-15 10:02:59 -0400

    Fully parses CC-CEDICT to JSON.

--- chineseToJson.js
+++ chineseToJson.js
@@ -1,18 +1,28 @@
+// Requires manual removal of final comma
 var fs = require('fs')
   , readline = require('readline')
-  , inFilePath = '../data/cedict_1_0_ts_utf-8_mdbg.txt'
+  , inFilePath = './res/cedict_1_0_ts_utf-8_mdbg.txt'
   , rl = readline.createInterface(fs.createReadStream(inFilePath), console.log)
-  , outFilePath = '../../test/out_02.json'
+  , outFilePath = '../test/out_01.json'
   , json = '{\n'
 
 rl.on('line', function (line) {
-  line = line.toString('utf8')
-  var chinese = escapeForJson(line.substring(0, line.indexOf(' ')))
-    , firstSlash = line.indexOf('/')
-    , secondSlash = line.indexOf('/', firstSlash + 1)
-    , english = escapeForJson(line.substring(firstSlash + 1, secondSlash))
-  json += '    "' + chinese + '": {\n'
-  json += '        "english": "' + english + '"\n'
+  var bkdn = breakdownLine(line.toString('utf8'))
+  if (!bkdn) { return; }
+  escapeForJson(bkdn)
+
+  json += '    "' + bkdn.trad + '": {\n'
+  json += '        "simp": "' + bkdn.simp + '",\n'
+  json += '        "pinyin": "' + bkdn.pinyin + '",\n'
+  json += '        "english": [\n'
+  for (var i = 0; i < bkdn.english.length; ++i) {
+    json += '            "' + bkdn.english[i] + '"'
+    if (i !== bkdn.english.length - 1) {
+      json += ','
+    }
+    json += '\n'
+  }
+  json += '        ]\n'
   json += '    },\n'
 });
 
@@ -26,6 +36,29 @@ rl.on('close', function () {
   })
 });
 
-function escapeForJson(str) {
+function breakdownLine(line) {
+  if (line[0] === '#') { return undefined; }
+  var lineRegex = /(.*?) (.*?) \[(.*?)\] \/(.*)\//
+    , breakdown = lineRegex.exec(line)
+  return { trad: breakdown[1]
+         , simp: breakdown[2]
+         , pinyin: breakdown[3]
+         , english: breakdown[4].split('/')
+  }
+}
+
+function escapeForJson(obj) {
+  for (var key in obj) {
+    if (typeof obj[key] === 'string') {
+      obj[key] = escapeOne(obj[key])
+    } else if (typeof obj[key] === 'object') {
+      for (var i in obj[key]) {
+        obj[key][i] = escapeOne(obj[key][i])
+      }
+    }
+  }
+}
+
+function escapeOne(str) {
   return str.replace(/"/g, '\\"')
 }
