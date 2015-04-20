35e434dc3fd4d7d09c16c8237453d45e8a00bd7c
pololanguage <polo.language@gmail.com>
2015-04-19 23:31:59 -0400

    Remote connection option added for building db.

--- db/fillDb.js
+++ db/fillDb.js
@@ -2,13 +2,42 @@ var MongoClient = require('mongodb')
   , fs = require('fs')
   , entries = JSON.parse(fs.readFileSync( './res/dict-json/dict_all.json'
                                         , { encoding: 'utf8' }))
-  , entry
-  , n = 0
+  , flag = process.argv[2]
+  , dbName = 'chinese-dictionary'
 
-MongoClient.connect('mongodb://localhost/', function (err, db) {
-  var myDb = db.db('chinese-dictionary')
-  myDb.dropCollection('DictEntries')
-  myDb.createCollection('DictEntries', function (err, nebulae) {
+if (flag === '-r' || flag === '--remote') {
+  connectRemote()
+} else if (flag === '-l' || flag === '--local') {
+  connectLocal()
+} else {
+  console.error('Usage:\n' +
+                'node db/fillDb --remote <username> <password>\n' +
+                'node db/fillDb --local')
+}
+
+function connectRemote() {
+  MongoClient.connect( 'mongodb://' + process.argv[3] + ':' + process.argv[4] +
+                       '@ds061518.mongolab.com:61518/' + dbName
+                     , function (err, db) {
+    if (err) return console.error(err);
+    fillCollection(db)
+    // setTimeout(function () { db.close(); }, 30000)
+  })
+}
+function connectLocal() {
+  MongoClient.connect('mongodb://localhost/', function (err, db) {
+    if (err) return console.error(err);
+    var myDb = db.db(dbName)
+    fillCollection(myDb) 
+    // setTimeout(function () { db.close(); }, 3000)
+  })
+}
+
+function fillCollection(db) {
+  var n = 0
+    , entry
+  db.dropCollection('DictEntries')
+  db.createCollection('DictEntries', function (err, nebulae) {
     for (var trad in entries) {
       entry = entries[trad]
       entry.key = n
@@ -16,13 +45,14 @@ MongoClient.connect('mongodb://localhost/', function (err, db) {
       addObject(nebulae, entry)
       ++n
     }
-    setTimeout(function () { db.close(); }, 3000)
   })
-})
+}
 
 function addObject(collection, obj) {
   collection.insert(obj, function (err, result) {
-    if (!err) {
+    if (err) {
+      console.log('Error: ' + err)
+    } else {
       console.log('Inserted: ' + result)
     }
   })
