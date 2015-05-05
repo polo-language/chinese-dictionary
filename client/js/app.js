64f13233d1ee74b76ad51cde3a9c9b5719629afa
Angelo Polo <polo.language@gmail.com>
2015-05-05 01:02:19 -0400

    Draft sort search results based on length. Draft add exact match option to search.

--- client/js/app.js
+++ client/js/app.js
@@ -12,6 +12,53 @@ app.factory('DictionarySvc', function($q, $http) {
     }
   }
 
+  function getSortMethod(lang, term) {
+    if (lang !== 'english') {
+      return function (a, b) {
+        if (a[lang].length < b[lang].length) {
+          return -1
+        } else if (a[lang].length > b[lang].length {
+          return 1
+        } else {
+          return 0
+        }
+      }
+    } else { // lang === 'english'
+      // TODO: change num word count length, not character length
+      return function (a, b) {
+        var aEng, bEng
+        // find (first) cell in array of English results containing search term
+        for (aEng = 0; aEng < a.english.length; ++aEng) {
+          if (a.english[aEng].search(term) > -1) {
+            break
+          }
+        }
+        for (bEng = 0; bEng < b.english.length; ++bEng) {
+          if (b.english[bEng].search(term) > -1) {
+            break
+          }
+        }
+        if (a.english[aEng].length < b.english[bEng].length) {
+          return -1
+        } else if (a.english[aEng].length > b.english[bEng].length {
+          return 1
+        } else {
+          return 0
+        }
+      }
+    }
+  }
+
+  function search(lang, term, query) {
+    var dfd = &q.defer()
+      , sortMethod = getSortMethod(lang, term)
+
+    $http.get('/api/search/' + lang + '/' + term + query).then(function (result) {
+      dfd.resolve(result.data.sort(sortMethod))
+    })
+    return dfd.promise
+  }
+
   return { getRandomEntries: getFromServer('/api/random/')
          , search: getFromServer('/api/search/')
   }
@@ -22,6 +69,7 @@ app.controller('EntryCtrl', function ($scope, DictionarySvc) {
   $scope.searchTerm = ''
   $scope.searchLang = 'english'
   $scope.wholeWord = true
+  $scope.exactMatch = false
 
   $scope.getRandom = function () {
     if (!$scope.numRandom) return
@@ -33,13 +81,27 @@ app.controller('EntryCtrl', function ($scope, DictionarySvc) {
   $scope.search = function () {
     if (!$scope.searchTerm) return
 
-    var query = '?wholeword=false'
+    var queries = []
+      , query = ''
+      , i = 0
     $scope.numRandom = ''
 
     if ($scope.wholeWord && $scope.searchLang === 'english') {
-      query = '?wholeword=true'
+      query[i] = 'wholeword=true'
+      ++i
     }
-    DictionarySvc.search($scope.searchLang + '/' + $scope.searchTerm + query).then(saveToEntries)
+    if ($scope.exactMatch) {
+      query[i] = 'exactmatch=true'
+      ++i
+    }
+    if (i !== 0) {
+      query = '?' + queries.join('&')
+    }
+
+    DictionarySvc.search( $scope.searchLang
+                        , $scope.searchTerm
+                        , query)
+                 .then(saveToEntries)
   }
 
   $scope.showAltEnglish = function(entry) {
