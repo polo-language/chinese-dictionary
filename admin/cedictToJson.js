95b4aa7e62a9878b49c76230d96c5aa31a2b3d74
Angelo Polo <language.devel@gmail.com>
2021-02-09 16:16:25 +0100

    Clean up dict to JSON conversion

--- admin/cedictToJson.js
+++ admin/cedictToJson.js
@@ -1,69 +1,53 @@
-// Requires manual removal of final comma
 const fs = require('fs')
 const readline = require('readline')
-const inFilePath = __dirname + '/res/CC-CEDICT/cedict_1_0_ts_utf-8_mdbg.txt'
-const rl = readline.createInterface(fs.createReadStream(inFilePath), console.log)
-const outFileDir = __dirname + '/res/dict-json'
-const outFilePath = outFileDir + '/dict_all.json'
-const json = '{\n'
 
-fs.mkdirSync(outFileDir)
+const LINE_REGEX = /(.*?) (.*?) \[(.*?)\] \/(.*)\//
+
+const inFilePath = process.argv[2]
+const outFilePath = process.argv[3]
+if (!inFilePath || !outFilePath) {
+  console.log(
+    'Usage:\n'+
+    'node admin/cedictToJson <input.txt> <output.json>'
+  )
+  throw new Error("Infile and outfile must both be provided as command-line arguments")
+}
+let json = '{\n'
+let isFirst = true
+const rl = readline.createInterface(fs.createReadStream(inFilePath), console.log)
 
 rl.on('line', function (line) {
-  const bkdn = breakdownLine(line.toString('utf8'))
-  if (!bkdn) { return; }
-  escapeForJson(bkdn)
-
-  json += '    "' + bkdn.trad + '": {\n'
-  json += '        "simp": "' + bkdn.simp + '",\n'
-  json += '        "pinyin": "' + bkdn.pinyin + '",\n'
-  json += '        "english": [\n'
-  for (let i = 0; i < bkdn.english.length; ++i) {
-    json += '            "' + bkdn.english[i] + '"'
-    if (i !== bkdn.english.length - 1) {
-      json += ','
+  const lineObj = lineToObj(line.toString('utf8'))
+  if (lineObj) {
+    if (!isFirst) {
+      json += ',\n'
     }
-    json += '\n'
+    isFirst = false
+    json += '"'+ lineObj.trad +'":' + JSON.stringify(lineObj)
   }
-  json += '        ]\n'
-  json += '    },\n'
 });
 
 rl.on('close', function () {
-  json += '}\n'
-  fs.open(outFilePath, 'w', function (err, fd) {
+  json += '\n}\n'
+  fs.writeFile(outFilePath, json, function (err) {
     if (err) {
-      return console.log(err)
+      console.error(err)
+    } else {
+      console.log(`Wrote JSON to ${outFilePath}`)
     }
-    fs.write(fd, json)
   })
 });
 
-function breakdownLine(line) {
-  if (line[0] === '#') { return undefined; }
-  const lineRegex = /(.*?) (.*?) \[(.*?)\] \/(.*)\//
-  const breakdown = lineRegex.exec(line)
-  return {
-    trad: breakdown[1],
-    simp: breakdown[2],
-    pinyin: breakdown[3],
-    english: breakdown[4].split('/')
-  }
-}
-
-// Modifies its argument
-function escapeForJson(obj) {
-  for (const key in obj) {
-    if (typeof obj[key] === 'string') {
-      obj[key] = escapeOne(obj[key])
-    } else if (typeof obj[key] === 'object') {
-      for (const i in obj[key]) {
-        obj[key][i] = escapeOne(obj[key][i])
-      }
+function lineToObj(line) {
+  if (line[0] === '#') {
+    return undefined;
+  } else {
+    const breakdown = LINE_REGEX.exec(line)
+    return {
+      trad: breakdown[1],
+      simp: breakdown[2],
+      pinyin: breakdown[3],
+      english: breakdown[4].split('/')
     }
   }
-}
-
-function escapeOne(str) {
-  return str.replace(/"/g, '\\"')
 }
